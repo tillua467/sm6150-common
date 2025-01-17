b645d2b945817cc8590f583b6fea4c9fd156fcec
tillua467 <tillua467@gmail.com>
2025-01-18 01:24:33 +0600

    Proper Bring Up for Los

--- sm6150.mk
+++ sm6150.mk
@@ -239,7 +239,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-infinity
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
