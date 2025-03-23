e8754049bac6254386d85840dafa5ccb5d105c26
tillua467 <tillua467@gmail.com>
2025-03-23 17:34:11 +0600

    Revert "sm6150-common: camera: Implement supportsSetTorchModeExt"
    This reverts commit b758f727d23184d2d5f64eda90b8346703ebdba3.

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -42,10 +42,6 @@ bool supportsTorchStrengthControlExt() {
     return true;
 }
 
-bool supportsSetTorchModeExt() {
-    return false;
-}
-
 int32_t getTorchDefaultStrengthLevelExt() {
     return 7;
 }
